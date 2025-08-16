9cef00835b93816d72221defd864f6a7ed08fb36
mertilhans <160387403+mertilhans@users.noreply.github.com>
2025-08-16 05:30:00 +0300

    Add files via upload

---
+++ Finish/execute_error_exit.c
@@ -0,0 +1,66 @@
+#include "shell.h"
+
+void	ft_directory(t_parser *cmd, t_exec_data *data)
+{
+	ft_putendl_fd("bash: ", 2);
+	ft_putendl_fd(cmd->argv[0], 2);
+	ft_putendl_fd(": is a directory\n", 2);
+	close_fd(data);
+	close_heredoc_fd(cmd);
+	gb_free_all();
+	env_gb_free_all();
+	exit(126);
+}
+
+void	ft_not_directory(t_parser *cmd, t_exec_data *data)
+{
+	ft_putendl_fd("bash: ", 2);
+	ft_putendl_fd(cmd->argv[0], 2);
+	ft_putendl_fd(": No such file or directory\n", 2);
+	close_fd(data);
+	close_heredoc_fd(cmd);
+	gb_free_all();
+	env_gb_free_all();
+	exit(127);
+}
+
+void	ft_not_executable(t_parser *cmd, t_exec_data *data, char *exec_path)
+{
+	if (!exec_path)
+	{
+		ft_putendl_fd("bash: ", 2);
+		ft_putendl_fd(cmd->argv[0], 2);
+		ft_putendl_fd(": command not found\n", 2);
+	}
+	else
+	{
+		ft_putendl_fd("bash: ", 2);
+		ft_putendl_fd(cmd->argv[0], 2);
+		ft_putendl_fd(": Permission denied\n", 2);
+	}
+	close_fd(data);
+	close_heredoc_fd(cmd);
+	gb_free_all();
+	env_gb_free_all();
+	exit(127);
+}
+
+void	ft_permission_denied(t_parser *cmd, t_exec_data *data)
+{
+	ft_putendl_fd("bash: ", 2);
+	ft_putendl_fd(cmd->argv[0], 2);
+	ft_putendl_fd(": Permission denied\n", 2);
+	close_fd(data);
+	close_heredoc_fd(cmd);
+	gb_free_all();
+	env_gb_free_all();
+	exit(126);
+}
+
+void	not_her_app_exp(t_heredoc_buffer *buf)
+{
+	perror("line error");
+	gc_free(buf->line);
+	gc_free(buf->content);
+	setup_interactive_signals();
+}
---
+++ Finish/execute_utils.c
@@ -0,0 +1,58 @@
+#include "shell.h"
+
+void	ft_putendl_fd(char *str, int fd)
+{
+	if (!str)
+		return ;
+	while (*str)
+		write(fd, str++, 1);
+}
+
+int	count_commands(t_parser *cmd)
+{
+	int	count;
+
+	count = 0;
+	while (cmd)
+	{
+		count++;
+		cmd = cmd->next;
+	}
+	return (count);
+}
+
+int	get_argv_count(char **argv)
+{
+	int	count;
+
+	count = 0;
+	while (argv[count])
+		count++;
+	return (count);
+}
+
+char	*copy_dir(const char *dir, int len)
+{
+	char	*res;
+	int		i;
+
+	i = 0;
+	res = gb_malloc(len + 1);
+	if (!res)
+		return (NULL);
+	while (i < len)
+	{
+		res[i] = dir[i];
+		i++;
+	}
+	res[len] = '\0';
+	return (res);
+}
+
+void	ft_free_and_close(t_exec_data *data, t_parser *cmd)
+{
+	close_fd(data);
+	close_heredoc_fd(cmd);
+	gb_free_all();
+	env_gb_free_all();
+}
